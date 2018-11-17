4b389a204ec2c6e9a994acbfe76744cb2ee2702c
roger.zhang@mail.mcgill.ca <roger.zhang@mail.mcgill.ca>
2018-11-17 13:57:39 -0500

    Added a table to passengers.vue

--- CarpoolApp-Web/src/components/Passengers.vue
+++ CarpoolApp-Web/src/components/Passengers.vue
@@ -1,6 +1,6 @@
 <template>
 
-<div id="carpoolMainPage">
+<div id="passengerPage">
 
 <!-- Navigation bar  -->
     <div class="topnav" id="main_nav">
@@ -15,12 +15,42 @@
 
 	</div>
    
+   <!-- Active participants table -->
+   <div class="passengerTable" id="passengerTableId">
+        <h1>List of Active Passengers</h1>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>ID</th>
+                <th>Averaige Price Paid Per Km</th>
+                <th>Total Distance Traveled</th>
+            </tr>
+            <tr>
+                <td>Roger Zhang</td>
+                <td>5</td>
+                <td>2</td>
+                <td>50000</td>
+            </tr>
+            <!-- <tr v-for="passenger in passengers">
+                <td>{{ passenger.name }}</td>
+                <td>{{ passenger.id }}</td>
+                <td>{{ passenger.averagePaidPerKm }}</td>
+                <td>{{ passenger.totalKm }}</td>
+            </tr> -->
+        </table>
+    </div>
+
+    <!-- Error -->
+    <span v-if="errorPassenger" style="color:red">Error: {{errorPassenger}} </span>
+
+
+
 </div>
 
 </template>
 
-<script>
-  // TODO
+<script >
+  
 </script>
 
 <style>
@@ -39,5 +69,29 @@
 .topnav a:hover {
   color: red;
 }
+
+/* Participants Table */
+.passengerTable h1 {
+    padding-top: 1em;
+    font-family: Helvetica, Arial;
+    font-size: 2em;
+    text-transform: uppercase;
+}
+
+.passengerTable table{
+    margin-top: 3em;
+    margin-left: auto;
+    margin-right:auto;
+    border: 1px solid black;
+}
+
+.passengerTable th, td{
+    font-family: Helvetica, Arial;
+    font-size: 1em;
+    padding: 0.5em;
+    border: 1px solid black;
+}
+
+
 
 </style>
