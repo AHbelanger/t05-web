de0a13892d0a3b02ce3793cb72933c938fea02b1
alexnorms <alexa.normandin@mail.mcgill.ca>
2018-11-18 15:52:52 -0500

    Routes tab and sorting active

--- CarpoolApp-Web/src/components/Routes.vue
+++ CarpoolApp-Web/src/components/Routes.vue
@@ -1,6 +1,6 @@
 <template>
-
-<div id="passengerPage">
+   
+<div id="routesPage">
 
 <!-- Navigation bar  -->
     <div class="topnav" id="main_nav">
@@ -14,39 +14,37 @@
 	  <router-link to="Routes">Routes</router-link>
 
 	</div>
-   
-   <!-- Active participants table -->
-   <div class="passengerTable" id="passengerTableId">
-        <h1>List of Active Passengers</h1>
+
+<!-- Active Routes Table -->
+    <div class="routeTable" id="routeTableId">
+        <h1>List of Active Routes</h1>
         <table id="myTable">
             <tr>
-                <th v-on:click="sortName">Name</th>
+                <th v-on:click="sortName">Driver Name</th>
                 <th v-on:click="sortID">ID</th>
-                <th v-on:click="sortPrice">Averaige Price Paid Per Km</th>
-                <th v-on:click="sortTotalKM">Total Distance Traveled</th>
+                <th v-on:click="sortstartPoint">Starting Point</th>
+                <th v-on:click="sortDestination">Destination</th>
             </tr>
-            <tr v-for="passenger in passengers">
-                <td>{{ passenger.name }}</td>
-                <td>{{ passenger.id }}</td>
-                <td>{{ passenger.averagePrice }}</td>
-                <td>{{ passenger.totalKM }}</td>
+            <tr v-for="route in routes">
+                <td>{{ route.driverName }}</td>
+                <td>{{ route.id }}</td>
+                <td>{{ route.startPoint }}</td>
+                <td>{{ route.destination }}</td>
             </tr>
-        </table>
-
+        </table>  
+    
     </div>
 
     <!-- Error -->
     <span v-if="errorPassenger" style="color:red">Error: {{errorPassenger}} </span>
 
 
-
 </div>
 
 </template>
 
-<script src="./passengerJs.js">
+<script src="./routesJs.js">
 </script>
-
 
 <style>
 /* Navigation bar */
@@ -64,34 +62,36 @@
   color: red;
 }
 
-/* Participants Table */
-.passengerTable h1 {
-  padding-top: 1em;
+/*Routes table*/
+.routeTable h1{
+    padding-top: 1em;
   font-family: Helvetica, Arial;
   font-size: 2em;
   text-transform: uppercase;
 }
 
-.passengerTable table {
-  margin-top: 3em;
+.routeTable table{
+     margin-top: 3em;
   margin-left: auto;
   margin-right: auto;
   border: 1px solid black;
 }
 
-.passengerTable th,
-td {
-  font-family: Helvetica, Arial;
+.routeTable th,
+td{
+    font-family: Helvetica, Arial;
   font-size: 1em;
   padding: 0.5em;
   border: 1px solid black;
 }
 
-.passengerTable th{
+.routeTable th{
     cursor:pointer;
 }
 
-.passengerTable th:hover{
+.routeTable th:hover{
     color: blue;
 }
+
 </style>
+
--- CarpoolApp-Web/src/router/index.js
+++ CarpoolApp-Web/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import CarpoolappHomePage from '@/components/CarpoolappHomePage'
 import Passengers from '@/components/Passengers'
+import Routes from '@/components/Routes'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/passengers',
       name: 'Passengers',
       component: Passengers
+    },
+    {
+      path: '/routes',
+      name: 'Routes',
+      component: Routes
     }
   ]
 })
