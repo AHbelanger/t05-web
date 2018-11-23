ebd9fc330eff22155f231accdf382bcb6a399c5a
roger.zhang@mail.mcgill.ca <roger.zhang@mail.mcgill.ca>
2018-11-23 00:20:32 -0500

    Changed date and time display in routes component

--- CarpoolApp-Web/src/components/routesJs.js
+++ CarpoolApp-Web/src/components/routesJs.js
@@ -62,21 +62,21 @@ export default {
         r1.startY = 33
         r1.endX = 7
         r1.endY = 34
-        r1.date = new Date("2018-11-20 5:10")
+        r1.date = new Date("2018-11-20 5:37")
 
         r2.driverName = "Antoine"
         r2.startX = 14
         r2.startY = 61
         r2.endX = 8
         r2.endY = 3
-        r2.date = new Date("2018-11-20 6:10")
+        r2.date = new Date("2018-11-20 6:42")
         
         r3.driverName = "Alexa"
         r3.startX = 4
         r3.startY = 6
         r3.endX = 10
         r3.endY = 12
-        r3.date = new Date("2018-10-20 5:10")
+        r3.date = new Date("2018-10-20 15:12")
 
         r4.driverName = "Roger"
         r4.startX = 53
